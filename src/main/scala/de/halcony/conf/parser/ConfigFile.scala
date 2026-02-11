de6c4266c2ac7f54fb890413b926bd987f2df4ae
simon <simon.koch@uibk.ac.at>
2026-02-11 09:20:05 +0100

    scalafmt

--- src/main/scala/de/halcony/conf/parser/ConfigFile.scala
+++ src/main/scala/de/halcony/conf/parser/ConfigFile.scala
@@ -1,26 +1,43 @@
 package de.halcony.conf.parser
 
 sealed trait ConfigFileElement {
-  val index      : Int
-  val lineNumber : Int
+  val index: Int
+  val lineNumber: Int
 }
 
-sealed case class ConfigFile(expressions : List[Expression], override val index : Int, override val lineNumber : Int) extends ConfigFileElement
+sealed case class ConfigFile(
+    expressions: List[Expression],
+    override val index: Int,
+    override val lineNumber: Int
+) extends ConfigFileElement
 
 sealed trait Expression extends ConfigFileElement
 
-sealed case class NameExpr(value : String, override val index : Int, override val lineNumber: Int) extends Expression
+sealed case class NameExpr(value: String, override val index: Int, override val lineNumber: Int)
+    extends Expression
 
-sealed case class ScalarExpr(value : String, override val index : Int, override val lineNumber: Int) extends Expression
+sealed case class ScalarExpr(value: String, override val index: Int, override val lineNumber: Int)
+    extends Expression
 
-sealed case class CommentExpr(value : String, override val index : Int, override val lineNumber : Int) extends Expression
+sealed case class CommentExpr(value: String, override val index: Int, override val lineNumber: Int)
+    extends Expression
 
-sealed case class ListExpr(values : List[Expression], override val index : Int, override val lineNumber: Int) extends Expression
+sealed case class ListExpr(
+    values: List[Expression],
+    override val index: Int,
+    override val lineNumber: Int
+) extends Expression
 
-sealed case class AssignmentExpr(name : Expression, value : Expression, override val index : Int, override val lineNumber : Int) extends Expression
+sealed case class AssignmentExpr(
+    name: Expression,
+    value: Expression,
+    override val index: Int,
+    override val lineNumber: Int
+) extends Expression
 
-sealed case class BlockExpr(name : Option[Expression],
-                            expressions : List[Expression],
-                            override val index : Int,
-                            override val lineNumber : Int) extends Expression
-
+sealed case class BlockExpr(
+    name: Option[Expression],
+    expressions: List[Expression],
+    override val index: Int,
+    override val lineNumber: Int
+) extends Expression
--- src/main/scala/de/halcony/conf/parser/Nginx.scala
+++ src/main/scala/de/halcony/conf/parser/Nginx.scala
@@ -7,41 +7,56 @@ import wvlet.log.LogSupport
 
 object Nginx extends LogSupport {
 
-  def process(content : String) : Option[ConfigFile] = {
+  def process(content: String): Option[ConfigFile] = {
     parse(content, parseFile(using _)) match {
       case Parsed.Success(value, index) => Some(value)
-      case failure: Parsed.Failure =>
+      case failure: Parsed.Failure      =>
         logger.error(failure)
         None
     }
   }
 
-  private[parser] def anyWhitespace[$ : P] = P(" " | "\t")
+  private[parser] def anyWhitespace[$: P] = P(" " | "\t")
 
-  private[parser] def parseNonBreakCharacter[$ : P] : P[String] = P(CharsWhile(!Set('\n',' ','\t',';').contains(_)).!)
+  private[parser] def parseNonBreakCharacter[$: P]: P[String] = P(
+    CharsWhile(!Set('\n', ' ', '\t', ';').contains(_)).!
+  )
 
-  private[parser] def parseFile[$ : P] : P[ConfigFile] = P(Start ~ parseExpr.rep ~ End).map(seq => ConfigFile(seq.toList,0,-1))
+  private[parser] def parseFile[$: P]: P[ConfigFile] =
+    P(Start ~ parseExpr.rep ~ End).map(seq => ConfigFile(seq.toList, 0, -1))
 
-  private[parser] def parseExpr[$ : P] : P[Expression] = P(
+  private[parser] def parseExpr[$: P]: P[Expression] = P(
     anyWhitespace.rep ~ (parseComment | parseAssignment | parseBlock)
   )
 
-  private[parser] def parseComment[$: P]: P[CommentExpr] = P(Index ~ "#" ~/ CharsWhile(_ != '\n').! ~ "\n".?)
-    .map((startIndex, comment) => CommentExpr(comment.trim, startIndex, -1)).log
+  private[parser] def parseComment[$: P]: P[CommentExpr] =
+    P(Index ~ "#" ~/ CharsWhile(_ != '\n').! ~ "\n".?)
+      .map((startIndex, comment) => CommentExpr(comment.trim, startIndex, -1)) // .log
 
-  private[parser] def parseName[$ : P] : P[NameExpr] = P(Index ~ parseNonBreakCharacter)
-    .map((indexStart,name) => NameExpr(name,indexStart,-1))//.log
+  private[parser] def parseName[$: P]: P[NameExpr] = P(Index ~ parseNonBreakCharacter)
+    .map((indexStart, name) => NameExpr(name, indexStart, -1)) // .log
 
-  private[parser] def parseScalar[$ : P] : P[ScalarExpr] = P(Index ~ parseNonBreakCharacter)
-    .map((startIndex,value) => ScalarExpr(value,startIndex,-1))//.log
+  private[parser] def parseScalar[$: P]: P[ScalarExpr] = P(Index ~ parseNonBreakCharacter)
+    .map((startIndex, value) => ScalarExpr(value, startIndex, -1)) // .log
 
-  private[parser] def parseScalarList[$ : P] : P[ListExpr] = P(Index ~ (parseScalar ~ " ".?).rep)
-    .map((indexStart,listExpr) => ListExpr(listExpr.toList,indexStart,-1))//.log
+  private[parser] def parseScalarList[$: P]: P[ListExpr] = P(Index ~ (parseScalar ~ " ".?).rep)
+    .map((indexStart, listExpr) => ListExpr(listExpr.toList, indexStart, -1)) // .log
 
-  private[parser] def parseAssignment[$ : P] : P[AssignmentExpr] = P(Index ~ parseName ~ !"{" ~ parseScalarList ~ ";")
-    .map((startIndex,name,value) => AssignmentExpr(name,if value.values.length == 1 then value.values.head else value,startIndex,-1))//.log
+  private[parser] def parseAssignment[$: P]: P[AssignmentExpr] =
+    P(Index ~ parseName ~ !"{" ~ parseScalarList ~ ";")
+      .map((startIndex, name, value) =>
+        AssignmentExpr(
+          name,
+          if value.values.length == 1 then value.values.head else value,
+          startIndex,
+          -1
+        )
+      ) // .log
 
-  private[parser] def parseBlock[$ : P] : P[BlockExpr] = P(Index ~ parseName ~ "{" ~ parseExpr.rep ~ "}")
-    .map((indexStart,nameExpr,exprs) => BlockExpr(Some(nameExpr),exprs.toList,indexStart,-1))//.log
+  private[parser] def parseBlock[$: P]: P[BlockExpr] =
+    P(Index ~ parseName ~ "{" ~ parseExpr.rep ~ "}")
+      .map((indexStart, nameExpr, exprs) =>
+        BlockExpr(Some(nameExpr), exprs.toList, indexStart, -1)
+      ) // .log
 
 }
