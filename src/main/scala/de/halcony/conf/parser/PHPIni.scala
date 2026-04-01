85533b5a31643cc2031e458c69b1101022fa693c
simon <simon.koch@uibk.ac.at>
2026-04-01 13:20:01 +0200

    now PHP ini parsing is supported

---
+++ src/main/scala/de/halcony/conf/parser/PHPIni.scala
@@ -0,0 +1,102 @@
+package de.halcony.conf.parser
+
+import fastparse.*
+import MultiLineWhitespace.*
+import fastparse.internal.Util
+import wvlet.log.LogSupport
+
+class PHPIni(content: String) extends LogSupport {
+
+  private def createLineIndex: Map[Int, Int] = {
+    var currentLine = 1
+    var currentChar = 0
+    this.content.map { char =>
+      try {
+        if (char == '\n') {
+          currentLine = currentLine + 1
+        }
+        currentChar -> currentLine
+      } finally {
+        currentChar = currentChar + 1
+      }
+    }.toMap
+  }
+
+  private val lineIndex: Map[Int, Int] = createLineIndex
+
+  def getLineIndex(charOffset: Int): Int = {
+    lineIndex.getOrElse(charOffset, -1)
+  }
+
+  def process(): Either[ConfigFile, fastparse.Parsed.Failure] = {
+    parse(this.content, parseFile(using _)) match {
+      case Parsed.Success(value, index) => Left(value)
+      case failure: Parsed.Failure      =>
+        logger.error(failure)
+        Right(failure)
+    }
+  }
+
+  private[parser] def anyWhitespace[$: P] = P(" " | "\t")
+
+  private[parser] def parseNonBreakCharacter[$: P]: P[String] = P(
+    CharsWhile(!Set('\n', ' ', '\t', ';', '=', '[', ']', '"', '\'').contains(_)).!
+  )
+
+  private[parser] def parseFile[$: P]: P[ConfigFile] =
+    P(Start ~ parsePHPDirective.rep ~ End).map(seq => ConfigFile(seq.toList, 0, getLineIndex(0)))
+
+  private[parser] def parsePHPDirective[$: P]: P[Expression] = P(
+    (parseComment | parseSection | parseDirective) ~ anyWhitespace.rep
+  )
+
+  private[parser] def parseComment[$: P]: P[CommentExpr] =
+    P(anyWhitespace.rep ~ Index ~ ";" ~~/ (CharsWhile(_ != '\n') | "").! ~~/ ("\n" | End))
+      .map((startIndex, comment) =>
+        CommentExpr(comment.trim, startIndex, getLineIndex(startIndex))
+      )
+
+  private[parser] def parseSection[$: P]: P[CommentExpr] = P(
+    anyWhitespace.rep ~ Index ~ "[" ~ CharsWhile(_ != ']').! ~ "]" ~ anyWhitespace.rep
+  ).map((startIndex, sectionName) =>
+    CommentExpr(sectionName, startIndex, getLineIndex(startIndex))
+  )
+
+  private[parser] def parseDirective[$: P]: P[CallExpr] = P(
+    anyWhitespace.rep ~ Index ~ parseNonBreakCharacter ~ anyWhitespace.rep ~ Index ~ "=" ~ anyWhitespace.rep ~ parsePHPValue.? ~ anyWhitespace.rep
+  ).map((startIndex, directiveName, equalsIndex, valueOption) =>
+    CallExpr(
+      NameExpr("=", equalsIndex, getLineIndex(equalsIndex)),
+      List(
+        NameExpr(directiveName, startIndex, getLineIndex(startIndex)),
+        valueOption.getOrElse(ScalarExpr("", equalsIndex, getLineIndex(equalsIndex)))
+      ),
+      startIndex,
+      getLineIndex(startIndex)
+    )
+  )
+
+  private[parser] def parsePHPValue[$: P]: P[Expression] = P(
+    Index ~ (parseQuotedString | parseNumber | parseExpression | parseNonBreakCharacter)
+  ).map { (startIndex, valueContent) =>
+    // For quoted strings, preserve the quotes in the ScalarExpr
+    // For other values (constants, etc.), use as-is
+    ScalarExpr(valueContent, startIndex, getLineIndex(startIndex))
+  }
+
+  private[parser] def parseQuotedString[$: P]: P[String] = P(
+    ("\"" ~ CharsWhile(_ != '"').! ~ "\"").map { content => s"\"$content\"" } |
+    ("'" ~ CharsWhile(_ != '\'').! ~ "'").map { content => s"'$content'" }
+  )
+
+  // Removed parseBooleanValue since we want to allow any unquoted constants
+  // via parseNonBreakCharacter
+
+  private[parser] def parseNumber[$: P]: P[String] = P(
+    ("-".? ~ CharsWhile(_.isDigit) ~ ("." ~ CharsWhile(_.isDigit)).? ~ CharsWhile(c => c.isLetter && c.isUpper)).!
+  )
+
+  private[parser] def parseExpression[$: P]: P[String] = P(
+    (parseNonBreakCharacter ~ ("&" | "|" | "^" | "~" | "!") ~ parseNonBreakCharacter).!
+  )
+}
---
+++ src/test/scala/de/halcony/conf/parser/PHPIniTest.scala
@@ -0,0 +1,179 @@
+package de.halcony.conf.parser
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import fastparse.*
+
+import java.io.File
+import java.nio.file.Files
+
+class PHPIniTest extends AnyWordSpec with Matchers {
+
+  "parsing a php.ini configuration file" should {
+    "work for single comment" in {
+      val conf = "; This is a comment"
+      new PHPIni(conf).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CommentExpr("This is a comment",0,1)
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for empty comment" in {
+      val comment = ";"
+      new PHPIni(comment).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CommentExpr("",0,1)
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for section header" in {
+      val section = "[PHP]"
+      new PHPIni(section).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CommentExpr("PHP",0,1)
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for simple directive" in {
+      val directive = "engine = On"
+      new PHPIni(directive).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CallExpr(
+              NameExpr("=",7,1),
+              List(
+                NameExpr("engine",0,1),
+                ScalarExpr("On",9,1)
+              ),
+              0,
+              1
+            )
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for directive with string value" in {
+      val directive = "user_ini.filename = \".user.ini\""
+      new PHPIni(directive).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CallExpr(
+              NameExpr("=",18,1),
+              List(
+                NameExpr("user_ini.filename",0,1),
+                ScalarExpr("\".user.ini\"",20,1)
+              ),
+              0,
+              1
+            )
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for directive with numeric value" in {
+      val directive = "max_execution_time = 30"
+      new PHPIni(directive).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CallExpr(
+              NameExpr("=",19,1),
+              List(
+                NameExpr("max_execution_time",0,1),
+                ScalarExpr("30",21,1)
+              ),
+              0,
+              1
+            )
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for directive with boolean value" in {
+      val directive = "display_errors = Off"
+      new PHPIni(directive).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CallExpr(
+              NameExpr("=",15,1),
+              List(
+                NameExpr("display_errors",0,1),
+                ScalarExpr("Off",17,1)
+              ),
+              0,
+              1
+            )
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+
+    "work for mixed content" in {
+      val mixedContent = """
+        |; PHP configuration file
+        |[PHP]
+        |engine = On
+        |display_errors = Off
+        |; End of configuration
+        |""".stripMargin
+
+      new PHPIni(mixedContent).process() match {
+        case Left(value) =>
+          value.expressions shouldBe List(
+            CommentExpr("PHP configuration file",1,2),
+            CommentExpr("PHP",26,3),
+            CallExpr(
+              NameExpr("=",39,4),
+              List(
+                NameExpr("engine",32,4),
+                ScalarExpr("On",41,4)
+              ),
+              32,
+              4
+            ),
+            CallExpr(
+              NameExpr("=",59,5),
+              List(
+                NameExpr("display_errors",44,5),
+                ScalarExpr("Off",61,5)
+              ),
+              44,
+              5
+            ),
+            CommentExpr("End of configuration",65,6)
+          )
+        case Right(value) =>
+          fail(value.toString())
+      }
+    }
+  }
+
+  "work with real php.ini file" in {
+    val file = new File("src/test/resources/php/php.ini")
+    new PHPIni(Files.readString(file.toPath)).process() match {
+      case Left(value) =>
+        // Just check that it parses successfully
+        value should not be null
+      case Right(value) =>
+        fail(s"file ${file.getPath} was not successfully parsed: $value")
+    }
+  }
+}
